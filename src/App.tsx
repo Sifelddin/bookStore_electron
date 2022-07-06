ef27d0868248be101a4a48144f17ab87dc9736b4
ali tarek <bekhouche3939@gmail.com>
2022-07-06 17:06:18 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center w-full bg-slate-200 h-screen">
+      <div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus ab asperiores dolorem! Vitae quae
+          exercitationem ea voluptatibus, dolore beatae. Itaque, quam nobis? Molestias aspernatur porro harum sunt
+          placeat perspiciatis optio?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
